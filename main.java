a316c72db8753858f952a8b64f258fbf9f528c9b
Meapri <pcw206041@gmail.com>
2021-04-01 11:35:14 +0900

    1부터 100까지의 합

---
+++ main.java
@@ -0,0 +1,17 @@
+package arabiannight.tistory.com.java.test;
+
+public class test {
+	
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int sum = 0;
+		
+		for(i = 1; i <= 100; i++){
+			sum += i;
+		System.out.println(sum);
+		}
+		
+		System.out.println(sum);
+	}
+}
